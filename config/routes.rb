5f71da951de276ded48803dbf94c3811d54d59a3
come mellerio <come.mellerio@free.fr>
2018-03-07 21:12:47 +0100

    ex1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :restaurants , only: [ :index, :show, :new, :create ] do
+      resources :reviews, only: [ :index, :new, :create ]
+    end
+    resources :reviews, only: [ :show, :edit, :update, :destroy ]
+end
+
+Rails.application.routes.draw do
+    namespace :admin do
+      resources :restaurants, only: [:index]
+    end
 end
